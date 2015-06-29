40504296cfd6d2dbedd7b5cf40fada5b4c3667b3
Burhan Dewantara <burhan.dewantara@artoncode.net>
2015-06-29 14:12:13 +0700

    Sound implemented

--- Assets/PlaySoundController.cs
+++ Assets/PlaySoundController.cs
@@ -5,10 +5,19 @@ using System.Collections.Generic;
 public class PlaySoundController : MonoBehaviour {
 
 	public List<AudioClip> sounds;
+	private AudioSource aSource;
 
+	public void Awake()
+	{
+		aSource = this.GetComponent<AudioSource> ();
+	}
 	public void PlaySound(int soundIndex)
 	{
-		Debug.Log("sound played "+ soundIndex);
+//		AudioSource.PlayClipAtPoint (sounds [soundIndex], Vector3.zero);
+		aSource.PlayOneShot (sounds [soundIndex]);
+//		aSource.clip = sounds [soundIndex];
+//		aSource.Play ();
+//		Debug.Log("sound played "+ soundIndex);
 
 	}
 }
---
+++ Assets/Scripts/Main/ScriptableObject/Editor/ProjectAssetPostprocessor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+class ProjectAssetPostprocessor : AssetPostprocessor {
+	void OnPreprocessAudio() {
+		if (assetPath.EndsWith(".wav") || assetPath.EndsWith(".mp3")) {
+		preprocessMusic();
+		}
+	}
+
+	void preprocessMusic() {
+		AudioImporter importer = (AudioImporter)assetImporter;
+
+		importer.format = AudioImporterFormat.Compressed;
+		importer.threeD = false;
+		importer.forceToMono = true;
+		importer.loadType = AudioImporterLoadType.DecompressOnLoad;
+		importer.hardware = false;
+		importer.loopable = true;
+		importer.compressionBitrate = 128000;
+	}
+}
