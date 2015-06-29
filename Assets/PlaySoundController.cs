40ee3a84297d1474920ede3e1f658bcac7c81362
dyelinne <dlyn.ryuu@gmail.com>
2015-06-29 12:54:13 +0700

    add sound library. blom semua. on going.

---
+++ Assets/PlaySoundController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlaySoundController : MonoBehaviour {
+
+	public List<AudioClip> sounds;
+
+	public void PlaySound(int soundIndex)
+	{
+		Debug.Log("sound played "+ soundIndex);
+
+	}
+}
