7ca8c87e71c08952d105964102ba8db25943a5b3
Burhan Dewantara <burhan.dewantara@artoncode.net>
2015-06-29 15:56:28 +0700

    story panel is work Perfectly.. with skip button

---
+++ Assets/TapToProgress.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Artoncode.Core;
+
+public class TapToProgress : MonoBehaviour,IInputManagerDelegate {
+
+	public int tapToProgress = 2;
+	public int tapCount = 0;
+
+	public GameObject textGameObject;
+	public string nextScene;
+
+	public void touchStateChanged (TouchInput []touches)
+	{
+
+		TouchInput input = touches [0];
+		Debug.Log (input);
+		Debug.Log (input.phase);
+		switch (input.phase) {
+		case TouchPhase.Began : 
+			Tapped();
+			break;
+		}
+	}
+
+
+	public void Tapped()
+	{
+		tapCount++;
+		if (tapCount < tapToProgress) {
+			textGameObject.SetActive (true);
+		} else {
+			ChangeScene();
+		}
+	}
+
+	public void ChangeScene()
+	{
+		LevelController.shared().LoadLevel(nextScene);
+	}
+}
